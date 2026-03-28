58fa7d10e984187347a08382fbd5f11f86651f92
Thalis Sousa <tatagamer677@gmail.com>
2026-03-27 22:30:11 -0300

    criando projeto

---
+++ MauiAppBemEstarDigital/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBemEstarDigital
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
